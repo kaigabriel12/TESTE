c1fad2afa2b3b828a350fa037a88aa0f03d067e8
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2022-10-31 15:24:02 -0300

    Add files via upload

--- SQL.py
+++ SQL.py
@@ -26,10 +26,16 @@ db = SQLAlchemy(app)
 
 # criação da classe com a estrutura da tabela Usuario
 class Dados(db.Model):
-    id_voto = db.column(db.Interger, primary_key=True)
+    id_voto = db.column(db.Integer, primary_key=True)
     data_hora = db.column(db.String(20))
     nome_modelo = db.column(db.String(20))
 
     def to_json(self):
         return {"id": self.id_voto, "data_hora": self.data_hora, "nome_modelo": self.nome_modelo }
-
+    
+@app.route('/insere')
+def inserir():
+    novo = Dados(id_voto=1,data_hora='10-10-2010',nome_modelo='cruz')
+    db.session.add(novo)
+    db.session.commit()
+    return
